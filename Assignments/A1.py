36f2e9d0de41c5d7f78a3969b86cb53bb21e8360
Robert Mitchell <robert@robertmitchellv.com>
2014-09-30 22:36:05 -0700

    Trying to add multiple q's and a's for print
    Not working…

--- Assignments/A1.py
+++ Assignments/A1.py
@@ -1,8 +1,12 @@
-# Bonjour tout le monde ! program.
+# Hello World Program
 def main():
-    #get the user's name (but ask in French!)
-    name = input('Comment vous vous appelez ?')
-    print("Bonjour tout le monde ! Je m'appelle", name)
+    #get the user's first name
+    givenName = input('What is your given name? ')
+    #get user's last name
+    surname = input('What is your surname? ')
+    #get the user's nickname
+    nickname = input('Tell me your nickname, alias, or super-secret name! ')
+    print("Hello there world; My name is", givenName, surname, "aka" nickname)
 
 # Call the main function
 main()
