368f351781a5bd9832bae93360c6869e60a09394
Robert Mitchell <robert@robertmitchellv.com>
2014-10-08 09:41:50 -0700

    Added basic variables and simple calculations
    Will likely need to revise for global constants and functions

---
+++ Assignments/A2.py
@@ -0,0 +1,16 @@
+# Fitness Club Diet Evaluation
+# Daily carb and fat gram calculator
+
+# Ask user to enter daily total of grams of fat consumed +
+# convert string to float for computation
+dailyFatGrams = flaot(input('Enter the fat grams consumed: '))
+
+# Ask user to enter daily total of grams of carbs consumed +
+# convert string to float for computation
+dailyCarbGrams = flaot(input('Enter the carbohydrate grams consumed: '))
+
+# Calculate fat calories
+fatCalories = dailyFatGrams * 9
+
+# Calculate carb calories
+carbCalories = dailyCarbGrams * 4
