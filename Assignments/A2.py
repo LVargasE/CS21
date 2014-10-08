cca837b876c14066a2db844b9d328f410c137579
Robert Mitchell <robert@robertmitchellv.com>
2014-10-08 10:00:51 -0700

    Completed A2 + fixed typos
    Done and working!

--- Assignments/A2.py
+++ Assignments/A2.py
@@ -3,14 +3,24 @@
 
 # Ask user to enter daily total of grams of fat consumed +
 # convert string to float for computation
-dailyFatGrams = flaot(input('Enter the fat grams consumed: '))
+dailyFatGrams = float(input('Enter the fat grams consumed: '))
 
 # Ask user to enter daily total of grams of carbs consumed +
 # convert string to float for computation
-dailyCarbGrams = flaot(input('Enter the carbohydrate grams consumed: '))
+dailyCarbGrams = float(input('Enter the carbohydrate grams consumed: '))
 
 # Calculate fat calories
 fatCalories = dailyFatGrams * 9
 
 # Calculate carb calories
 carbCalories = dailyCarbGrams * 4
+
+# Display results of computation with function showAll
+def showAll():
+    print('Grams of fat: ', dailyCarbGrams)
+    print('Fat calories: ', fatCalories)
+    print('Grams of carbs: ', dailyFatGrams)
+    print('Carb calories: ', carbCalories)
+
+# Call function
+showAll()
