f4dadfe7c7a6a247a15a1b87b18513abc13e3c8b
Robert Mitchell <robert@robertmitchellv.com>
2014-10-12 14:30:09 -0700

    Added instructions in comments to A3

---
+++ Assignments/A3.py
@@ -0,0 +1,47 @@
+# Software Sales
+#
+# INFO:
+#
+# Companies will create software and then set a retail price that the end user
+# will pay.  In order to get retailers to buy in large quantities and then be
+# able to make a profit, the creator of the product will offer quantity
+# discounts.  Usually, the more you buy, the larger the discount.
+#
+# These can be assembled into a table that will show the quantities and the
+# corresponding discounts.
+#
+# We will be using a function and some decision structures.
+#
+# The Program Spec:
+#
+# A software company sells a package that retails for $99.  Quantity discounts
+# are given according to the following table:
+#
+# Quantity	Discount
+# 10 - 19	20%
+# 20 - 49	30%
+# 50 - 99	40%
+# 100 or more	50%
+#
+# The company will ask customers to enter the number of packages that they
+# want to purchase.  The program will should then display the amount of the
+# discount, if any, and the total amount of the purchase.
+#
+# 1 - Create a global constant for the retail price of the package
+# 2 - Create a main that asks the user for the number of packages and then
+#   calculates the discount and total for the purchase in the order specified
+#   above.
+# 3 - Create a function named showPurchase(discountAmount, totalAmount): that
+#   will
+#   print out the discount and total amount with titles.
+# 4 - Be sure to include comments in your code.
+# 5 - Run the program at least 4 times times with different numbers of
+#   packages to show that your discounts are working and include it as output
+#   with your program.
+#
+# Should look like:
+# >>>
+# Enter the number of packages purchased: 4
+# Discount Amount: $ 0.00
+# Total Amount: $ 396.00
+# >>>
