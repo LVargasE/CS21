417c9cc20b613890e006d9a79ccaf23dfb8e688d
Robert Mitchell <robert@robertmitchellv.com>
2014-10-16 16:06:13 -0700

    Working on A3
    unfinished

--- Assignments/A3.py
+++ Assignments/A3.py
@@ -1,47 +1,42 @@
-# Software Sales
-#
-# INFO:
-#
-# Companies will create software and then set a retail price that the end user
-# will pay.  In order to get retailers to buy in large quantities and then be
-# able to make a profit, the creator of the product will offer quantity
-# discounts.  Usually, the more you buy, the larger the discount.
-#
-# These can be assembled into a table that will show the quantities and the
-# corresponding discounts.
-#
-# We will be using a function and some decision structures.
-#
-# The Program Spec:
-#
-# A software company sells a package that retails for $99.  Quantity discounts
-# are given according to the following table:
-#
-# Quantity	Discount
-# 10 - 19	20%
-# 20 - 49	30%
-# 50 - 99	40%
-# 100 or more	50%
-#
-# The company will ask customers to enter the number of packages that they
-# want to purchase.  The program will should then display the amount of the
-# discount, if any, and the total amount of the purchase.
-#
-# 1 - Create a global constant for the retail price of the package
-# 2 - Create a main that asks the user for the number of packages and then
-#   calculates the discount and total for the purchase in the order specified
-#   above.
-# 3 - Create a function named showPurchase(discountAmount, totalAmount): that
-#   will
-#   print out the discount and total amount with titles.
-# 4 - Be sure to include comments in your code.
-# 5 - Run the program at least 4 times times with different numbers of
-#   packages to show that your discounts are working and include it as output
-#   with your program.
-#
-# Should look like:
-# >>>
-# Enter the number of packages purchased: 4
-# Discount Amount: $ 0.00
-# Total Amount: $ 396.00
-# >>>
+# Assignment 3
+# Software Sales Program
+
+# Global constant for retail price of software
+RETAIL_PRICE = 99
+# Global constant for discounts to be used in computation
+DISCOUNT_20 = .2
+DISCOUNT_30 = .3
+DISCOUNT_40 = .4
+DISCOUNT_50 = .5
+
+# Main Module
+def main():
+
+    # Get user to input the amount of packages
+    packageQuantity = int(input("Enter the number of packages purchased: "))
+
+    # conditional structure to determine discount amount
+    if packageQuantity >= 10 and packageQuanity <= 19:
+      discountAmount = parckageQuantity * DISCOUNT_20
+    else:
+      if packageQuantity >= 20 and packageQuantity <= 49:
+        discountAmount = parckageQuantity * DISCOUNT_30
+      else:
+        if packageQuantity >= 50 and packageQuantity <= 99:
+          discountAmount = parckageQuantity * DISCOUNT_40
+        else:
+          if packageQuantity >= 100:
+            discountAmount = parckageQuantity * DISCOUNT_50
+
+    # Calculate total amount
+    totalAmount = discountAmount - (packageQuantity * RETAIL_PRICE)
+
+    showPurchase(discountAmount, totalAmount)
+ 
+def showPurchase(discountAmount, totalAmount):
+    print("Enter the number of packages purchase: ", packageQuantity)
+    print("Discount Amount: ", discountAmount)
+    print("Total Amount: $", totalAmount)
+
+# Call the main function
+main()
