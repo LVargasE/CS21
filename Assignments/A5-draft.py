7657c441857c60d71881d9043bba8cf14625ba46
Robert Mitchell <robert@robertmitchellv.com>
2014-10-25 14:33:17 -0700

    starting on A5

---
+++ Assignments/A5-draft.py
@@ -0,0 +1,17 @@
+# Rock, Paper, Scissors
+#
+# use random number (1-3)
+# don't show the computer's number!
+# have user enter their choice
+# display the computer's choice as a word; not a number
+# winner selected
+# winner congratulated
+#
+# program specs
+# --> main() calls functions and passes arguments
+# --> create value returning function named rockPaperScissors(computer, player)
+#     that receives numbers for the user/player and computer
+# --> value returning function named choiceString(choice)
+
+import random
+import turtle
