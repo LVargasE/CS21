23f832404235e2b489d57fd6569c317f3b0bc8d0
Robert Mitchell <robert@robertmitchellv.com>
2014-11-06 11:10:06 -0800

    testing dictionary use

--- Assignments/A6-test.py
+++ Assignments/A6-test.py
@@ -14,9 +14,42 @@ import os
 
 choice = ''
 found = ''
+records = {}
 
-def displayRecords(var):
-        
+def loadRecords(k, y):
+  # open the original Golfers.txt file to print all records
+  infile = open("Golfers.txt", "r")
+
+  # read the first record's 'name' field
+  k = infile.readline()
+
+  # read rest of file
+  while k != '':
+      # read the score field
+      v = infile.readline()
+
+      # strip the '\n' from 'name'
+      k = k.rstrip('\n')
+      v = v.rstrip('\n')
+
+      # send name and score to dictionary
+      records["k"] = v
+
+      # read next name
+      k = infile.readline()
+
+    # close file
+    infile.close()
+
+def displayRecords():
+  print('Golfers Records:\n')
+
+  # call function to load records
+  loadRecords(name, score)
+
+  # print all records
+  for (name, score) in records.items():
+    print("Golfer's Name: ", name, "\tGolfer's Score: ", score)
 
 def addRecords(var):
 
@@ -24,10 +57,12 @@ def lookUpRecords(var):
 
 def removeRecords(var):
 
-def loadRecords(var):
-
 def saveRecords(var):
 
 def getMenu():
 
 def main():
+  loadRecords(name, score)
+  displayRecords()
+
+main()
