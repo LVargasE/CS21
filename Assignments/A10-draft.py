fae4e3dfedee629a11213fec77a83847cfad6e2a
robertmitchellv <robert.beaty.mitchell@gmail.com>
2014-12-04 23:54:56 -0800

    finished program A10

--- Assignments/A10-draft.py
+++ Assignments/A10-draft.py
@@ -7,7 +7,61 @@ import csv
 import random
 
 class Question:
-    pass
+    def __init__(self, question, a1, a2, a3, a4, answer, ansNum):
+        self.getData = Data('csv')
+        self.question = question
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        self.a4 = a4
+        self.answer = answer
+        self.ansNum = ansNum
+    
+    @classmethod
+    def getQuestion(cls, triviaDict):
+        # using random to get 10 random numbers between a specific range for 
+        # trivia questions
+        randomGenerator = random.sample(range(1, 817), 1)
+        
+        # for an individual random number in the sample range 
+        # --> iterate and use number as index for the trivia questions
+        for i in randomGenerator:
+            question = triviaDict[i][0]
+            a1 = triviaDict[i][1]
+            a2= triviaDict[i][2]
+            a3 = triviaDict[i][3]
+            a4 = triviaDict[i][4]
+            answer = triviaDict[i][5]
+            ansNum = triviaDict[i][6]
+        
+        aQuestion = Question(question, a1, a2, a3, a4, answer, ansNum)
+        return aQuestion
+        
+    def __getattr__(self, attr):
+        return getattr(self.getData, attr)
+    
+    @classmethod
+    def setupAsk(cls, q):
+        print('\n', q.question, '\n\t1: ', q.a1, ' \
+                 \n\t2: ', q.a2, '\n\t3: ', q.a3, ' \
+                 \n\t4: ', q.a4)
+        
+        while True:
+            try:
+                choice = int(input("\nWhat's your answer? \n--> "))
+            except ValueError:
+                print('Sorry, the answer only accepts numbers; please \
+                       enter a number 1-4')
+            finally:
+                if choice in range(1, 5):
+                    break
+            
+        if choice == q.ansNum:
+            print('Correct! \n', q.answer)
+            return True
+        elif choice != q.ansNum:
+            print('Incorrect!  \n', q.answer)
+            return False   
 
 class Data:
     def __init__(self, filetype):
@@ -24,49 +78,99 @@ class Data:
                 
                 # questions, answer choices, and answers dictionary
                 rowDict = {}
-            
+                questionData = {}
                 # reading the trivia questions
                 for row in readCSV:
                     rowDict[index] = row
-                    print(type(row))
-                    print(type(rowDict))
-                    """
-                    question = rowDict[0]
-                    a1 = rowDict[1]
-                    a2 = rowDict[2]
-                    a3 = rowDict[3]
-                    a4 = rowDict[4]
-                    answer = rowDict[5]
+                    question = rowDict[index][0]
+                    a1 = rowDict[index][1]
+                    a2 = rowDict[index][2]
+                    a3 = rowDict[index][3]
+                    a4 = rowDict[index][4]
+                    answer = rowDict[index][5]
                     
                     # figure out which answer is correct and assign a variable to it
                     if answer == a1:
-                        ansNum = rowDict.append(1)
+                        ansNum = 1
                     elif answer == a2:
-                        ansNum = rowDict.append(2)
+                        ansNum = 2
                     elif answer == a3:
-                        ansNum = rowDict.append(3)
+                        ansNum = 3
                     elif answer == a4:
-                        ansNum = rowDict.append(4)
+                        ansNum = 4
                     else:
                         print("Error!  No correct answer")
                     # place questions into new dictionary in the right order
-                    rowDict[index] = [question, a1, a2, a3, a4, \
+                    questionData[index] = [question, a1, a2, a3, a4, \
                                             answer, ansNum]
-                    """
-                    return rowDict
+                    index += 1
+
+                return questionData
                     
         except IOError:
-            print("The file could not be found.")
-            
+            print("The file could not be found.")         
 
 class Game:
-    def __init__(self, gamePoints, totalPoints):
+    def __init__(self, playerID, gamePoints, totalPoints):
+        self.playerID = playerID
         self.gamePoints = gamePoints
         self.totalPoints = totalPoints
-
+    
+    def round(self, qClass, data, totalPoints):
+        gamePoints = 0
+        
+        q1 = qClass.getQuestion(data)
+        q2 = qClass.getQuestion(data)
+        q3 = qClass.getQuestion(data)
+        q4 = qClass.getQuestion(data)
+        q5 = qClass.getQuestion(data)
+        
+        if qClass.setupAsk(q1) == True:
+            gamePoints = 1
+            totalPoints += 1
+        if qClass.setupAsk(q2) == True:
+            gamePoints += 1
+            totalPoints += 1
+        if qClass.setupAsk(q3) == True:
+            gamePoints += 1
+            totalPoints += 1
+        if qClass.setupAsk(q4) == True:
+            gamePoints += 1
+            totalPoints += 1
+        if qClass.setupAsk(q5) == True:
+            gamePoints += 1
+            totalPoints += 1
+            
+        print('you won {} points this game and have {} total \
+               points from previous games.'.format(gamePoints, totalPoints))
+        
+        return totalPoints
+        
 def main():
+    totalPointsP1 = 0
+    totalPointsP2 = 0
+    
     data = Data('csv')
     questionsData = data.getData()
-    print(questionsData)
-
+    
+    questions = Question('question', 'a1', 'a2', 'a3', 'a4', \
+                         'answer', 'ansNum')
+                         
+    print('\nROUND ONE//\nPlayer One')
+    playerOne = Game(str(input('Enter your name: ')), 0, 0)
+    p1round1 = playerOne.round(questions, questionsData, totalPointsP1)
+    
+    print('\nSwitch Players\n')
+    
+    print('\nROUND ONE//\nPlayer Two')
+    playerTwo = Game(str(input('Enter your name: ')), 0, 0)
+    p2round1 = playerTwo.round(questions, questionsData, totalPointsP2)
+    
+    if p1round1 < p2round1:
+        print('{} is the winner with {} total \
+              points!'.format(playerTwo.playerID, p2round1))
+    elif p2round1 < p1round1:
+        print('{} is the winner with {} total \
+              points!'.format(playerOne.playerID, p1round1))
+    
 main()
--- Assignments/A10.py
+++ Assignments/A10.py
@@ -1,62 +1,17 @@
 # -*- coding: utf-8 -*-
-"""   A10--Trivia Game! 
-      --> two player trivia game
-      --> OOP approach to building the game with classes and objects
+"""     A10--Trivia Game!
+        --> two player trivia game
+        --> OOP approach to building the game with classes and objects
 """
 import csv
 import random
-               
-# the Data class enables the main function to pass the data from a CSV file 
-# through methods that place the data in a dictionary with numbers as keys 
-# and a list as the value
-class Data():
-    # the init method has one attribupte and other methods that andable the
-    # main funciton to affect the object to do the data janitorial work 
-    # necessary for processing in the game
-    def __init__(self, triviaData):
-        self.triviaData = triviaData
-    
-    # uses the CSV dictionary and separates questions into keys (numbers for
-    # questions) and values (a list that contains all the Question class
-    # attributes that aren't parsed)
-    def getQuestions(self, triviaData):
-        # local variables
-        index = 0
-        questionsDict =  {}
-        
-        # iterates through all the data to organize into a nested list
-        for i in range(1, 816):
-            question = triviaData[i][0]
-            a1 = triviaData[i][1]
-            a2= triviaData[i][2]
-            a3 = triviaData[i][3]
-            a4 = triviaData[i][4]
-            answer = triviaData[i][5]
-            
-            # figure out which answer is correct and assign a variable to it
-            if answer == a1:
-                ansNum = 1
-            elif answer == a2:
-                ansNum = 2
-            elif answer == a3:
-                ansNum = 3
-            elif answer == a4:
-                ansNum = 4
-            else:
-                print("Error!  No correct answer")
-          
-            # place questions into new dictionary in the right order
-            questionsDict[index] = [question, a1, a2, a3, a4, answer, ansNum]
-            index += 1
 
-        return questionsDict
-        
-# the Game class class allows for easy parsing of question attributes from
-# nested lists for easier handling and processing         
-class Game(Data):
-    def __init__(self, triviaData, question, a1, a2, a3, a4, answer, ansNum, \
-                 ansCorrect, ansIncorrect, roundWins):
-        Data.__init__(self, triviaData)
+# the Question class acts as a placeholder for the parts of the question
+# needed to construct questions and check answers
+class Question:
+    # __init__ uses the Data class method getData through composition
+    def __init__(self, question, a1, a2, a3, a4, answer, ansNum):
+        self.getData = Data('csv')
         self.question = question
         self.a1 = a1
         self.a2 = a2
@@ -64,116 +19,225 @@ class Game(Data):
         self.a4 = a4
         self.answer = answer
         self.ansNum = ansNum
-        self.ansCorrect = ansCorrect
-        self.ansIncorrect = ansIncorrect
-        self.roundWins = roundWins                                                       
 
-    # method for parsing a single question and create an instance from the 
-    # Question class to return to main
-    def parseQuestions(self, aDict, qNum):
-        for i in aDict[qNum]:
-            question = aDict[qNum][0]
-            a1 = aDict[qNum][1] 
-            a2 =aDict[qNum][2]
-            a3 = aDict[qNum][3]
-            a4 = aDict[qNum][4]
-            answer = aDict[qNum][5]
-            ansNum = aDict[qNum][6] 
-            
-            # instantiates the object
-            aTriviaQuestion = Game(question, a1, a2, a3, a4, answer, ansNum)
+    # the method performs better as a class method since it instantiates the
+    # class
+    @classmethod
+    def getQuestion(cls, triviaDict):
+        # using random to get 10 random numbers between a specific range for
+        # trivia questions
+        randomGenerator = random.sample(range(1, 817), 1)
 
-        return aTriviaQuestion
-    
-    def setUpQuestions(self, triviaData, question, a1, a2, a3, a4, answer, ansNum, \
-                 ansCorrect, ansIncorrect, roundWins):
+        # for an individual random number in the sample range
+        # --> iterate and use number as index for the trivia questions
+        for i in randomGenerator:
+            question = triviaDict[i][0]
+            a1 = triviaDict[i][1]
+            a2= triviaDict[i][2]
+            a3 = triviaDict[i][3]
+            a4 = triviaDict[i][4]
+            answer = triviaDict[i][5]
+            ansNum = triviaDict[i][6]
 
-        # call the method from Data class to get all the questions
-        triviaData = data.getQuestions(data.triviaData)
-        
-        # use the random module's sample method to select five random numbers
-        randomGen = random.sample(range(1, 817), 5)
-        
-        # instantiates by calling the Data class method parseQuestions 
-        qOne = data.parseQuestions(triviaData, randomGen[0])
-        qTwo = data.parseQuestions(triviaData, randomGen[1])
-        qThree = data.parseQuestions(triviaData, randomGen[2])
-        qFour = data.parseQuestions(triviaData, randomGen[3])
-        qFive = data.parseQuestions(triviaData, randomGen[4])
-        
-        # set up an instance of Game for player one
-        playerOne = Game(qOne.triviaData, question, qOne.a1, qOne.a2, qOne.a3,\
-                         qOne.a4, qOne.answer, qOne.ansNum, 0, 0, 0)
-        
-        p1aCorr, p1aInCorr = playerOne.askQuestions(qOne.question, qOne.a1,\
-                                                    qOne.a2, qOne.a3, qOne.a4,\
-                                                    qOne.answer, qOne.ansNum,\
-                                                    0, 0, 0)
-    
-    def askQuestions(self, question, a1, a2, a3, a4, answer, ansNum, \
-                     ansCorrect, ansIncorrect, roundWins):
+        # this creates an instance to return (from question class)
+        aQuestion = Question(question, a1, a2, a3, a4, answer, ansNum)
+        return aQuestion
 
-        print('THE SCORE SO FAR//\n\n\t Rounds Won: ', roundWins, '\n')
-        
-        print('\nFIRST QUESTION//\n', question, '\n\t1: ', a1, ' \
-                 \n\t2: ', a2, '\n\t3: ', a3, ' \
-                 \n\t4: ', a4)
-           
+    # this is a part of using composition rather than inheritance to get the
+    # attributes
+    def __getattr__(self, attr):
+        return getattr(self.getData, attr)
+
+    # this method sets up the question (also checks answer)
+    @classmethod
+    def setupAsk(cls, q):
+        print('\n', q.question, '\n\t1: ', q.a1, ' \
+                 \n\t2: ', q.a2, '\n\t3: ', q.a3, ' \
+                 \n\t4: ', q.a4)
+
+        # make sure the user's input works
         while True:
             try:
                 choice = int(input("\nWhat's your answer? \n--> "))
             except ValueError:
                 print('Sorry, the answer only accepts numbers; please \
                        enter a number 1-4')
+                choice = int(input("\nWhat's your answer? \n--> "))
             finally:
                 if choice in range(1, 5):
                     break
-           
-            if choice == ansNum:
-                ansCorrect += 1
-            elif choice != ansNum:
-                ansIncorrect += 1
-            
-        return ansCorrect, ansIncorrect
 
-# main function        
-def main():
-    # instantiate the Data class and assign the variable 'data'
-    data = Data(getData())
-    
-    print('NOW SWITCH PLAYERS')
-    
-    # set up an instance of Game for player two
-    playerTwo = Game(qOne.question, qOne.a1, qOne.a2, qOne.a3, \
-                     qOne.a4, qOne.answer, qOne.ansNum, 0, 0, 0)
-    
-    p2aCorr, p2aInCorr = playerOne.askQuestions(qOne.question, qOne.a1, \
-                                                qOne.a2, qOne.a3, qOne.a4,  \
-                                                qOne.answer, qOne.ansNum, \
-                                                0, 0, 0)
-            
-    
-# function outside of class to open the file and close the file where the 
-# trivia questions are housed.      
-def getData():
+        # if the question is correct, return true; if not, return false
+        if choice == q.ansNum:
+            print('Correct! \n', q.answer)
+            return True
+        elif choice != q.ansNum:
+            print('Incorrect!  \n', q.answer)
+            return False
+
+# the Data class handles openning the file and preparing it to be used by
+# the Question and Game class
+class Data:
+    def __init__(self, filetype):
+        self.filetype = filetype
+
+    # opens the CSV to read and prepare it to be used in computaiton later
+    @classmethod
+    def getData(cls):
         # make sure there isn't an IO error
         try:
             # open the csv file + use an index accumulator for dictionary
             with open('trivia.csv') as csvFile:
                 readCSV = csv.reader(csvFile, delimiter=',')
                 index = 0
-                
+
                 # questions, answer choices, and answers dictionary
                 rowDict = {}
-            
+                questionData = {}
                 # reading the trivia questions
                 for row in readCSV:
                     rowDict[index] = row
+                    question = rowDict[index][0]
+                    a1 = rowDict[index][1]
+                    a2 = rowDict[index][2]
+                    a3 = rowDict[index][3]
+                    a4 = rowDict[index][4]
+                    answer = rowDict[index][5]
+
+                    # figure out which answer is correct and assign a variable to it
+                    if answer == a1:
+                        ansNum = 1
+                    elif answer == a2:
+                        ansNum = 2
+                    elif answer == a3:
+                        ansNum = 3
+                    elif answer == a4:
+                        ansNum = 4
+                    else:
+                        print("Error!  No correct answer")
+
+                    # place questions into new dictionary in the right order
+                    questionData[index] = [question, a1, a2, a3, a4, \
+                                            answer, ansNum]
                     index += 1
-    
+
+                return questionData
+
         except IOError:
             print("The file could not be found.")
-        
-        return rowDict
-        
+
+# the Game class is to
+class Game:
+    def __init__(self, playerID, gamePoints, totalPoints):
+        self.playerID = playerID
+        self.gamePoints = gamePoints
+        self.totalPoints = totalPoints
+
+    # method to create instances for questions and find out if a quesiton
+    # was answered correctly or not
+    def round(self, qClass, data, totalPoints):
+        gamePoints = 0
+
+        # instances
+        q1 = qClass.getQuestion(data)
+        q2 = qClass.getQuestion(data)
+        q3 = qClass.getQuestion(data)
+        q4 = qClass.getQuestion(data)
+        q5 = qClass.getQuestion(data)
+
+        # return value is true or false; this computes points
+        if qClass.setupAsk(q1) == True:
+            gamePoints += 1
+            totalPoints += 1
+        if qClass.setupAsk(q2) == True:
+            gamePoints += 1
+            totalPoints += 1
+        if qClass.setupAsk(q3) == True:
+            gamePoints += 1
+            totalPoints += 1
+        if qClass.setupAsk(q4) == True:
+            gamePoints += 1
+            totalPoints += 1
+        if qClass.setupAsk(q5) == True:
+            gamePoints += 1
+            totalPoints += 1
+
+        # let the user know what happenned
+        print('you won {} points this game and have {} total \
+               points from previous games.'.format(gamePoints, totalPoints))
+
+        return totalPoints
+
+def main():
+    # local variables
+    flag = False
+    gameNum = 1
+    totalPointsP1 = 0
+    totalPointsP2 = 0
+
+    # instance of Data class
+    data = Data('csv')
+    questionsData = data.getData()
+
+    # instance of Question class with filler data
+    questions = Question('question', 'a1', 'a2', 'a3', 'a4', \
+                         'answer', 'ansNum')
+
+    # create both players
+    playerOne = Game(str(input('PLAYER ONE//\nEnter your name: ')), 0, totalPointsP1)
+    playerTwo = Game(str(input('PLAYER TWO//\nEnter your name: ')), 0, totalPointsP2)
+
+    # while loop to keep the game going if the user chooses
+    while flag != True:
+        # let the user know which round they're playing
+        print('\nROUND ', gameNum, '//\nPlayer One')
+
+        # first player instance; asks five questions
+        p1round = playerOne.round(questions, questionsData, totalPointsP1)
+
+        print("""
+        +++++++++++++++++++++++++++++++++++++++++++++++++
+        +                   SWITCH PLAYERS!             +
+        +++++++++++++++++++++++++++++++++++++++++++++++++
+        """)
+
+        # let the user know to switch players
+        print('\nROUND ', gameNum, '//\nPlayer Two')
+
+        # second player instance; asks the five quesitons
+        p2round = playerTwo.round(questions, questionsData, totalPointsP2)
+
+        gameNum += 1
+
+        if p1round < p2round:
+            print('{} is the winner with {} total \
+                  points!'.format(playerTwo.playerID, p2round))
+        elif p2round < p1round:
+            print('{} is the winner with {} total \
+                  points!'.format(playerOne.playerID, p1round))
+
+        while True:
+            try:
+                choice = str(input("\nKeep playing? \n--> ")).upper()
+            except ValueError:
+                print("Sorry, enter either a 'Y' for 'Yes', or 'N' for 'No'.")
+            finally:
+                if choice == 'N':
+                    flag = True
+                    break
+                if choice == 'Y':
+                    break
+                else:
+                    print("please enter either a 'Y' for 'Yes', or 'N' \
+                           for 'No'.")
+    # figure out who won
+    if p1round < p2round:
+        print('Thank you for playing!  {} is the winner with {} total \
+              game wins!'.format(playerTwo.playerID, p2round))
+    elif p2round < p1round:
+        print('Thank you for playing!  {} is the winner with {} total \
+              game wins!'.format(playerOne.playerID, p1round))
+    elif p2round == p1round:
+        print('There was a tie!  But you are both still winners!')
+
 main()
